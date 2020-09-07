b9ea139bc609ac2e66f12d517b7227ec935fc8a6
Süleyman Kardaş <skardas@gmail.com>
2020-09-07 03:31:00 +0300

    Moved voting contract

---
+++ contracts/core/Voting.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.8;
+
+import "../lib/SafeMath.sol";
+
+// TODO: update this contract
+contract Voting {
+
+    using SafeMath for uint256;
+
+    enum Vote {Accept, Reject}
+
+    struct Proposal {
+        bytes32 name;
+        uint256 accepts;
+        uint256 rejects;
+        uint256 endTime;
+        mapping (address => Vote) votes;
+    }
+
+    Proposal[] private _proposals;
+
+    // TODO only governance contract can call this
+    function propose(bytes32 name) public returns(bool) {
+        Proposal memory proposal;
+        proposal.name = name;
+        proposal.endTime = block.timestamp.add(2 days); // TODO: it can be dynamic.
+
+        return true;
+    }
+}
