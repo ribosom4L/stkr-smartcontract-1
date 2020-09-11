4f868ab76780ecd3a696686ce9c93e11f0259d28
Süleyman Kardaş <skardas@gmail.com>
2020-09-12 00:11:51 +0300

    buidler config example updated

--- buidler.config.example.js
+++ buidler.config.example.js
@@ -1,4 +1,5 @@
 usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("@nomiclabs/buidler-web3");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
