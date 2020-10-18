0449a8b0f9ad23c044f2acd89889e7c369b4c16b
4n1L <4n1l@protonmail.com>
2020-10-18 05:17:32 +0300

    Added micropool creation tests, deposit contract simulation for tests

--- migrations/1599864726_micropool.js
+++ migrations/1599864726_micropool.js
@@ -1,18 +1,26 @@
 const Micropool = artifacts.require('Micropool')
 const TokenContract = artifacts.require('AETH')
 const SystemParameters = artifacts.require('SystemParameters')
+const DepositContract = artifacts.require('DepositContract')
 
 const { deployProxy } = require('@openzeppelin/truffle-upgrades')
 
 module.exports = async (deployer) => {
   const tokenContract = await TokenContract.deployed()
   const parameters = await SystemParameters.deployed()
-  // TODO: env
-  const beaconAddr = '0x07b39F4fDE4A38bACe212b546dAc87C58DfE3fDC'
+
+  let beaconAddr
+
+  if (process.env.NETWORK === 'LOCAL') {
+    await deployer.deploy(DepositContract)
+    beaconAddr = (await DepositContract.deployed()).address
+  } else {
+    beaconAddr = process.env.DEPOSIT_CONTRACT
+  }
 
   const micropool = await deployProxy(Micropool, [tokenContract.address, parameters.address, beaconAddr], {
     deployer,
-    // TODO: structs to mappings
+    // TODO: Check the upgrades if structs fit our upgrades
     unsafeAllowCustomTypes: true
   })
 
