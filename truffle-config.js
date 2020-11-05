eff5d054e8d6da392b63a550b62304a6aa8ec14e
4n1L <4n1l@protonmail.com>
2020-11-05 21:55:45 +0300

    Stkr pool updates

--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,11 @@ const privateKey       = process.env.DEPLOYMENT_KEY;
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*",
-    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 8545,
+    //   network_id: "*",
+    // },
     develop: {
       port: 8545,
       defaultEtherBalance: 5000,
