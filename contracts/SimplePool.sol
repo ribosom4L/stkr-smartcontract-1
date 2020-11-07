9da39b95cfec3a0636fb5837b2762680ecf25364
4n1L <4n1l@protonmail.com>
2020-11-07 18:36:07 +0300

    Pool progress

--- contracts/SimplePool.sol
+++ contracts/SimplePool.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.6.8;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/Math.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
 import "./lib/interfaces/IDepositContract.sol";
@@ -17,30 +18,58 @@ interface IStkrPool {
 
 contract SimplePool is OwnableUpgradeSafe, Lockable {
     using SafeMath for uint256;
+    using Math for uint256;
 
     event StakePending(address indexed staker, uint256 amount);
     event StakeConfirmed(address indexed staker, uint256 amount);
 
     /* pool events */
-//    event PoolPushWaiting(bytes32 indexed pool); // we dont have pending pools anymore
-    event PoolOnGoing(bytes32 indexed pool);
-    event PoolCompleted(bytes32 indexed pool);
-//    event PoolClosed(bytes32 indexed pool);
+    //    event PoolPushWaiting(bytes32 indexed pool); // we dont have pending pools anymore
+    event PoolOnGoing(bytes indexed pool);
+    event PoolCompleted(bytes indexed pool);
+    //    event PoolClosed(bytes32 indexed pool);
 
     event ProviderExited(address indexed provider, uint256 exitBlock);
+    event ProviderANKRSlash(address indexed provider, uint256 ankrAmount, uint256 etherEquivalence);
+    event ProviderETHSlash(address indexed provider, uint256 amount);
 
-    mapping (address => uint256) _pendingUserStakes;
+    event TopUpETH(address indexed provider, uint256 amount);
+    event TopUpANKR(address indexed provider, uint256 amount);
+
+    mapping(address => uint256) private _pendingUserStakes;
+    mapping(address => uint256) private _userStakes;
+
+    mapping(address => uint256) private _etherBalances;
+    mapping(address => uint256) private _slashings;
+
+    mapping (address => uint256) private _exits;
+
+    // Pending staker list
     address[] private _pendingStakers;
+    // total pending amount
     uint256 private _pendingAmount;
-
-    mapping (address => uint256) private _userStakes;
+    // total stakes of all users
     uint256 private _totalStakes;
-
+    // total rewards for all stakers
     uint256 private _totalRewards;
-    uint256 private _totalSlashings;
+
+    IAETH private _aethContract;
+
+    IStaking private _stakingContract;
+
+    SystemParameters private _systemParameters;
+
+    address _depositContract;
+
+    modifier notExitRecently(address provider) {
+        require(block.number > _exits[provider].add(_systemParameters.EXIT_BLOCKS()), "Recently exited");
+        _;
+    }
 
     function initialize() public initializer {
         __Ownable_init();
+
+        _depositContract = 0x00000000219ab540356cBB839Cbe05303d7705Fa;
     }
 
     function pushToBeacon(uint256 poolIndex,
@@ -48,8 +77,10 @@ contract SimplePool is OwnableUpgradeSafe, Lockable {
         bytes memory withdrawal_credentials,
         bytes memory signature,
         bytes32 deposit_data_root) public onlyOwner {
+
         require(_pendingAmount >= 32 ether, "pending ethers not enough");
-        IDepositContract(_depositContract).deposit{value : ethersToSend}(pubkey, withdrawal_credentials, signature, deposit_data_root);
+
+        IDepositContract(_depositContract).deposit{value : 32 ether}(pubkey, withdrawal_credentials, signature, deposit_data_root);
 
         // substract 32 ether from pending amount
         _pendingAmount = _pendingAmount.sub(32 ether);
@@ -71,39 +102,112 @@ contract SimplePool is OwnableUpgradeSafe, Lockable {
                 emit StakeConfirmed(staker, userStake.sub(remained));
                 break;
             }
-            emit StakeConfirmed(staker, userStake);
             _pendingUserStakes[staker] = 0;
             i++;
+            emit StakeConfirmed(staker, userStake);
+        }
+
+        uint256[] memory newPendingArray;
+
+        // we should remove stakers from pending array length is: i
+        for (uint256 j = i; j < _pendingUserStakes.length; j++) {
+            newPendingArray.push(_pendingUserStakes[j]);
         }
         // we are deleting
         delete _pendingStakers;
+        delete _pendingUserStakes;
+        _pendingUserStakes = newPendingArray;
+
         emit PoolOnGoing(deposit_data_root);
     }
 
-    function stake() public payable {
-        if (_pendingStakes[msg.sender] == 0) {
-            _pendingStakers.push(msg.sender);
-        }
-
-        _pendingStakes[msg.sender] = _pendingStakes[msg.sender].add(msg.value);
-        _pendingAmount = _pendingAmount.add(msg.value);
-
-        _userStakes[msg.sender] = _userStakes[msg.sender].add(msg.value);
-
-        emit StakePending(msg.sender, msg.value);
+    function stake() notExitRecently(msg.sender) public payable {
+        _stake(msg.sender, msg.value);
     }
 
-    /**topUpETH*/
+    function _stake(address staker, uint256 value) private {
+        require(value > 0, "Value must be greater than zero");
+        if (_pendingStakes[staker] == 0) {
+            _pendingStakers.push(staker);
+        }
 
-    /**topUpANKR*/
+        _pendingStakes[staker] = _pendingStakes[staker].add(value);
+        _pendingAmount = _pendingAmount.add(value);
 
-    /**slashProviderETH*/
+        _userStakes[staker] = _userStakes[staker].add(value);
 
-    /**slashProviderANKR*/
+        emit StakePending(staker, value);
+    }
 
-    /**claimAeth*/
+    function topUpETH() public notExitRecently(msg.sender) payable {
+        _etherBalances[msg.sender] = _etherBalances[msg.sender].add(msg.value);
+        _stake(msg.value, msg.sender);
+        emit TopUpETH(msg.sender, msg.value);
+    }
 
-    /**providerExit*/
+    function topUpANKR(uint256 amount) public notExitRecently(msg.sender) payable {
+        /* Approve ankr & freeze ankr */
+        require(_stakingContract.freeze(msg.sender, amount), "Could not freeze ANKR tokens");
+        emit TopUpANKR(amount, msg.value);
+    }
+
+    // slash provider with ethereum balance
+    function slash(address provider, uint256 amount) public onlyOwner {
+        require(amount > 0, "Amount should be greater than zero");
+        uint256 remaining = _slashETH(provider, amount);
+
+        if (remaining == 0) {
+            return;
+        }
+
+        remaining = _slashANKR(remaining);
+        /*Do we need event if remaining balance higher than zero?*/
+    }
+
+    function claimableAETH() public view returns (uint256) {
+        return 0;
+    }
+
+    /**
+        Slash eth, returns remaining needs to be slashed
+    */
+    function _slashETH(address provider, uint256 amount) private returns (uint256 remaining) {
+        uint256 toBeSlashed = amount.min(_availableEtherBalanceOf(provider));
+        _slashings[provider] = _slashings[provider].add(toBeSlashed);
+        remaining = amount.sub(toBeSlashed);
+
+        emit ProviderETHSlash(provider, toBeSlashed);
+    }
+
+    function _slashANKR(address provider, uint256 amount) private returns (uint256 remaining) {
+//        _stakingContract.compensatePoolLoss()
+//        emit ProviderANKRSlash(provider, ankrAmount, amount);
+        remaining = 0;
+    }
+
+    function providerExit() public {
+        _exits[msg.sender] = block.number;
+    }
+
+    function claimAETH() {
+        /*...*/
+    }
+
+    function _availableEtherBalanceOf(address provider) private view returns (int256) {
+        return _etherBalanceOf(provider) - _slashingsOf(provider);
+    }
+
+    function _etherBalanceOf(address provider) private view returns (uint256) {
+        return _etherBalances[provider];
+    }
+
+    function _slashingsOf(address provider) private view returns (uint256) {
+        return _slashings[provider];
+    }
+
+    function _ankrBalanceOf(address provider) private view returns (uint256) {
+        return _stakingContract.frozenStakesOf(provider);
+    }
 
     function poolCount() public view returns (uint256) {
         return _totalStakes % 32;
