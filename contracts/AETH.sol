5a5df30985167eafe710bf58541780f65b8e0d5a
4n1L <4n1l@protonmail.com>
2020-11-08 04:33:55 +0300

    Removed old contracts, AETH ratio implementation, now its simplifed, StkrPool finished (Ankr staking needs to be finished in staking contract)

--- contracts/AETH.sol
+++ contracts/AETH.sol
@@ -9,11 +9,7 @@ import "./lib/Lockable.sol";
 contract AETH is OwnableUpgradeSafe, ERC20UpgradeSafe, Lockable {
     using SafeMath for uint256;
 
-    event Freeze(address indexed account, uint256 value);
-    event Unfreeze(address indexed account, uint256 value);
-    event Claimed(address payable user, uint256 amount);
-
-    event ClaimableToggle(bool _claimable);
+    event RatioUpdate(uint256 newRatio);
 
     uint256 private _totalSupply;
 
@@ -21,20 +17,14 @@ contract AETH is OwnableUpgradeSafe, ERC20UpgradeSafe, Lockable {
     string private _symbol;
     uint8 private _decimals;
 
-    mapping(address => uint256) private _balances;
-    mapping(address => uint256) private _frozenBalances;
+    address private _microPoolContract;
 
-    bool public _claimable;
-
-    address public _microPoolContract;
+    // ratio should be base on 1 ether
+    // if ratio is 0.9, this variable should be  9e17
+    uint256 private _ratio;
 
     modifier onlyMicroPoolContract() {
         require(_microPoolContract == _msgSender(), "Ownable: caller is not the micropool contract");
-        _;
-    }
-
-    modifier claimable() {
-        require(_claimable, "Not claimable");
         _;
     }
 
@@ -43,74 +33,28 @@ contract AETH is OwnableUpgradeSafe, ERC20UpgradeSafe, Lockable {
         __ERC20_init(name, symbol);
         _totalSupply = 0;
 
-        _claimable = false;
+        _ratio = 1e18;
+    }
+
+    function updateRatio(uint256 newRatio) public onlyOwner {
+        require(newRatio < _ratio, "New ratio cannot be smaller than old ratio");
+        _ratio = newRatio;
+        emit RatioUpdate(_ratio);
+    }
+
+    function ratio() public view returns (uint256) {
+        return _ratio;
     }
 
     function updateMicroPoolContract(address microPoolContract) external onlyOwner {
         _microPoolContract = microPoolContract;
     }
 
-    function availableBalanceOf(address account) public view returns (uint256) {
-        return balanceOf(account).sub(frozenBalanceOf(account));
-    }
-
-    function frozenBalanceOf(address account) public view returns (uint256) {
-        return _frozenBalances[account];
-    }
-
     function mint(address account, uint256 amount) external onlyMicroPoolContract {
-        _mint(account, amount);
-    }
-
-    function mintPool() payable external onlyMicroPoolContract {
-        _mint(msg.sender, msg.value);
-    }
-
-    function mintFrozen(address account, uint256 amount) external onlyMicroPoolContract {
-        _frozenBalances[account] = _frozenBalances[account].add(amount);
-
-        _mint(account, amount);
-
-        emit Freeze(account, amount);
-    }
-
-    function unfreeze() external claimable {
-        uint256 amount = _frozenBalances[msg.sender];
-
-        require(amount > 0, "Frozen Balance zero");
-
-        _frozenBalances[msg.sender] = 0;
-
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
-
-        emit Unfreeze(msg.sender, amount);
-    }
-
-    function swap() payable claimable external {
-        require(availableBalanceOf(msg.sender) > 0, "Available aeth balance is zero");
-
-        uint256 balance = availableBalanceOf(msg.sender);
-        _burn(msg.sender, balance);
-        require(msg.sender.send(balance), "Insufficient funds");
+        _mint(account, amount.mul(ratio).div(1e18));
     }
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
-    }
-
-    function toggleClaimable() external onlyOwner {
-        _claimable = !_claimable;
-        emit ClaimableToggle(_claimable);
-    }
-
-    receive() external payable {
-        _mint(msg.sender, msg.value);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) override internal {
-        // if this is a real transfer
-        if (from != address(0)) {
-            require(availableBalanceOf(from) >= amount, "Available balance is lower than transfer amount");
-        }
     }
 }
