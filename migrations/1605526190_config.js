3e6f690e3160c7fd5986ea100df4cfedcd20db69
4n1L <4n1l@protonmail.com>
2020-11-18 11:23:09 +0300

    migrations cleared

--- migrations/1605526190_config.js
+++ migrations/1605526190_config.js
@@ -4,8 +4,5 @@ const GlobalPool_R1 = artifacts.require('GlobalPool_R1')
 const GlobalPool = artifacts.require('GlobalPool')
 
 module.exports = async (deployer) => {
-  const configContract = await deployProxy(Config, [], { deployer });
-
-  const instance = new web3.eth.Contract(GlobalPool_R1.abi, GlobalPool.address)
-  await instance.methods.updateConfigContract(configContract.address)
+  await deployProxy(Config, [], { deployer });
 };
