7e9d294691afd65c60d8c28d6c4a3c9b4d564977
4n1L <4n1l@protonmail.com>
2020-11-26 20:59:26 +0300

    aeth upgrade cleanup

--- migrations/1606209264_change_aeth_tick.js
+++ migrations/1606209264_change_aeth_tick.js
@@ -7,12 +7,5 @@ const AETH_R2 = artifacts.require('AETH_R2');
 module.exports = async function (deployer, accounts) {
   const existing = await AETH.deployed();
   const instance = await upgradeProxy(existing.address, AETH_R2, { deployer });
-  await instance.changeSymbol("aETH")
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
-  console.log(await instance.symbol())
+  await instance.changeSymbolAndName("aETH", "aEther")
 };
