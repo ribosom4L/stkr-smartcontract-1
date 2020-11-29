e932cf8381dbe6e2253e8e5529f7c1975e2f7bd5
4n1L <4n1l@protonmail.com>
2020-11-29 21:16:07 +0300

    update migrations for global_pool_r18

--- migrations/1606588068_global_pool_upgrade__r18.js
+++ migrations/1606588068_global_pool_upgrade__r18.js
@@ -5,8 +5,8 @@ const GlobalPool = artifacts.require('GlobalPool');
 const GlobalPool_R16 = artifacts.require('GlobalPool_R16');
 
 module.exports = async function (deployer, accounts) {
-  const existing = await GlobalPool.deployed();
-  const instance = await upgradeProxy(existing.address, GlobalPool_R16, { deployer });
-  // open claiming aeth
-  await instance.togglePause(web3.utils.fromAscii('claim'))
+  // const existing = await GlobalPool.deployed();
+  // const instance = await upgradeProxy(existing.address, GlobalPool_R16, { deployer });
+  // // open claiming aeth
+  // await instance.togglePause(web3.utils.fromAscii('claim'))
 };
