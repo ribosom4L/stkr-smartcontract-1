4e6d4a14e34f8b82c1ed1069689779b580c3fe2e
4n1L <4n1l@protonmail.com>
2020-11-30 16:08:12 +0300

    improvments on globalpool

--- migrations/1606588079_global_pool_upgrade__r19.js
+++ migrations/1606588079_global_pool_upgrade__r19.js
@@ -2,13 +2,11 @@ const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 const Config      = artifacts.require("Config");
 
 const GlobalPool = artifacts.require('GlobalPool');
-const GlobalPool_R18 = artifacts.require('GlobalPool_R18');
+const GlobalPool_R19 = artifacts.require('GlobalPool_R19');
 
 module.exports = async function (deployer, accounts) {
   if (deployer.network === 'mainnet') return;
 
   const existing = await GlobalPool.deployed();
-  const instance = await upgradeProxy(existing.address, GlobalPool_R18, { deployer });
-  // open claiming aeth
-  await instance.togglePause(web3.utils.fromAscii('claim'))
+  const instance = await upgradeProxy(existing.address, GlobalPool_R19, { deployer });
 };
