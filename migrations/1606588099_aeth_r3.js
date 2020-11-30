850947d74ac3aaa154ae15670ae85398d5ee0d06
4n1L <4n1l@protonmail.com>
2020-11-30 20:43:51 +0300

    fix claim problem with tests

--- migrations/1606588099_aeth_r3.js
+++ migrations/1606588099_aeth_r3.js
@@ -7,7 +7,5 @@ const AETH_R3 = artifacts.require("AETH_R3");
 module.exports = async function(deployer, accounts) {
   const existing = await AETH.deployed();
   const instance = await upgradeProxy(existing.address, AETH_R3, { deployer });
-  if (deployer.network === "mainnet")
-
-    await instance.changeSymbolAndName("ankrETH", "Ankr ETH");
+  await instance.changeSymbolAndName("ankrETH", "Ankr ETH");
 };
